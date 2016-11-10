1ce396212cb12ef004fc059900d2aaa56bf885f3
marineco <marinecolonge@gmail.com>
2016-11-10 16:50:09 +0100

    bonus conversion rgba to hex fonctionnel

--- app.js
+++ app.js
@@ -20,8 +20,16 @@
 			$('body').css('backgroundColor', 'rgba('+r+', '+g+', '+b+', '+a+')');
 
 			$('#result').html(r + ', ' + g + ', ' + b + ', ' + a);
+			
 
-		}
+			function rgbaToHex(r, g, b, a) {
+				if (r > 255 || g > 255 || b > 255 || a > 255)
+					throw "Invalid color component";
+				return ((r << 32) | (g << 16) | (b << 8) | a).toString(16);
+			}
+			console.log(rgbaToHex(r, g, b, a));
+			
+		},
 	};
 
 	$(document).ready(function() {
