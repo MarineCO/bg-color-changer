d23f79deb7dbbd2af38717a32c840732b548e93f
marineco <marinecolonge@gmail.com>
2016-11-11 20:23:45 +0100

    ajout new version conversion rgba to hex plus simple + refactorisation code

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 		},
 
 		listeners: function() {
-			$('.range').on('input', this.getVal);
+			$('.range').on('input', this.getVal.bind(this));
 		},
 
 		getVal: function() {
@@ -16,12 +16,33 @@
 			var g = $('#g').val();
 			var b = $('#b').val();
 			var a = $('#a').val();
+			this.updateView(r, g, b, a);
+		},
 
+		updateView: function(r, g, b, a) {
+			this.rgba(r, g, b, a);
+			this.rgbaToHex(r, g, b, a);
+		},
+
+		rgba: function(r, g, b, a) {
 			$('body').css('backgroundColor', 'rgba('+r+', '+g+', '+b+', '+a+')');
 
-			$('#result').html(r + ', ' + g + ', ' + b + ', ' + a);
+			$('#result').html('RGBA :' + ' ' + r + ', ' + g + ', ' + b + ', ' + a);
+		},
 
-		}
+		rgbaToHex: function (r, g, b, a) {
+			var redColor = parseInt(r, 10);
+			var greenColor = parseInt(g, 10);
+			var blueColor = parseInt(b, 10);
+			var transparence = parseInt(a, 10);
+
+			var redHex = redColor.toString(16);
+			var greenHex = greenColor.toString(16);
+			var blueHex = blueColor.toString(16);
+			var transpHex = transparence.toString(16);
+
+			$('#result2').html('HEX :' + ' ' + '#' + redHex + greenHex + blueHex + transpHex);
+		},
 	};
 
 	$(document).ready(function() {
